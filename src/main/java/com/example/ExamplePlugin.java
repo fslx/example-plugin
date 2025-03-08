5857560710fb31e4616bc0aa3aee221f8076068d
fslx <51758889+fslx@users.noreply.github.com>
2025-03-08 12:43:26 +0000

    inital commit

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import com.google.inject.Provides;
+
+import java.util.Map;
+
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
@@ -50,4 +53,43 @@ public class ExamplePlugin extends Plugin
 	{
 		return configManager.getConfig(ExampleConfig.class);
 	}
+	/**
+	 * Map of the most desireable tasks 
+	 */
+	public static final Map<String, Double> slayerTaskPropabiliesMap = map.of(
+		"Abyssal Demons", 0.05,
+		"Nechryael", 0.03,
+		"Greater Demons", 0.08,
+		"Dust Devils", 0.07,
+		"Smoke Devils", 0.02,
+		"Kraken", 0.01,
+		"Hydra", 0.015,
+		"Cerberus", 0.012,
+		"Dagannoth Kings", 0.025,
+		"Lizardman Shaman", 0.018,
+		"Skeletal Wyverns", 0.04
+	);
+	/**
+	 * 
+	 * @param event
+	 */
+	@Subscribe
+	public void onAssignedSlayerTask(SlayerTaskAssigned event) {
+		String assignedTask = event.getTask().getName();
+		double propability = slayerTaskPropabiliesMap.getOrDefault(event, 0.0);
+		if(propability > 0) {
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The propability of recieving this task was: " + propability, null);
+		}
+	}
+
+	/**
+	 * List of the best/most desireable slayer tasks
+	 * @return List<String>
+	 */
+	public static final List<String> listOfBestSlayerTasks = list.of(
+		"Abyssal Demons", "Nechryaels", "Dust Devils", "Smoke Devils", "Black Demons", "Araxxyte",
+		"Kraken", "Hydra", "Cerberus", "Dagannoth Kings",
+    	"Smoke Devils", "Lizardman Shaman", "Skeletal Wyverns"
+	);
+	
 }
